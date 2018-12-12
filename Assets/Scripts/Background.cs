8c54d70d18665246218298b9106cf2efdc869873
Alexander Nigl <theonlynigl@gmail.com>
2018-12-11 20:57:48 -0600

    Game update

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    private BoxCollider2D groundCollider;
+    private float groundHorizontalLength;
+    
+	void Awake ()
+    {
+        groundCollider = GetComponent<BoxCollider2D>();
+        groundHorizontalLength = groundCollider.size.x;
+
+        transform.position = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Star: MonoBehaviour
+{
+    private Rigidbody2D Body { get; set; }
+
+    private void Awake() {
+        Body = GetComponent<Rigidbody2D>();
+    }
+
+    public void AddForceWithDirection(Vector2 direction) {
+        Body.AddForce(direction);
+    }
+
+    public void AddForceWithDirectionAndSpeed(Vector2 direction, float speed) {
+        Body.AddForce(direction * speed);
+    }
+
+    private void SetPosition(){
+        Body.position = Camera.main.ScreenToWorldPoint(Vector3.zero);
+    }
+}
