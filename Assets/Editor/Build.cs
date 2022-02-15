20d8c71686f5abcfb4dd42e16a289a5e6b6a2090
Alexander James Nigl <theonlynigl@gmail.com>
2022-02-14 21:15:36 -0500

    Build Hierarchy and Scene Open Part 1
    A build tool addition, and start
    Integrating functionality to control the scene Hierarchy on unity open
    Both share a resource of the constructed scene layout

---
+++ Assets/Editor/Build.cs
@@ -0,0 +1,17 @@
+﻿namespace Assets.Editor
+{
+    internal class Build
+    {
+        public static Build build { get; set; }
+
+        public void BuildProject()
+        {
+
+        }
+
+        private void SetSceneHierarchy()
+        {
+
+        }
+    }
+}
---
+++ Assets/Editor/BuildMage.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor
+{
+    public class BuildMage : EditorWindow
+    {
+        [MenuItem("Tools/Build Mage")]
+        static void init()
+        {
+            // Get existing open window or if none, make a new one:
+            BuildMage window = (BuildMage)GetWindow(typeof(BuildMage));
+            window.Show();
+        }
+
+        void OnGui()
+        {
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Build"))
+            {
+                Build.build.BuildProject();
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+}
